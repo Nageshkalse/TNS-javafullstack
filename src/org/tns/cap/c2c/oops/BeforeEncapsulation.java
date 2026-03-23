ffe6685e313c5a1f88fff810a8e7fe51fa20a0fb
Nageshkalse <nageshkalse144@gmail.com>
2026-03-23 18:47:15 +0530

    firstcommit

--- src/org/tns/cap/c2c/oops/BeforeEncapsulation.java
+++ src/org/tns/cap/c2c/oops/BeforeEncapsulation.java
@@ -1,10 +1,22 @@
 package org.tns.cap.c2c.oops;
 
+class Human
+{
+	 int age;
+	 String name;
+}
 public class BeforeEncapsulation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Human human=new Human();
+		human.age=21;
+		human.name="Akash";
+		human.age=55;
+		human.name="Neha";
+		human.age=33;
+		human.name="Sushma";
+		System.out.println("Name "+human.name);
+		System.out.println("Age "+human.age);
 
 	}
-
 }
