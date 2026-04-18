0603b572aac5a7f6b40a0de61e7d08d716b2ca91
Nagesh Kalse <nageshkalse144@gmail.com>
2026-04-18 15:38:08 +0530

    Delete src/org/tns/cap directory

--- src/org/tns/cap/c2tc/jrdeveloper/AirBag.java
+++
@@ -1,8 +0,0 @@
-package org.tns.cap.c2tc.jrdeveloper;
-
-public class AirBag {
-	public void showAirBag() {
-        System.out.println("Airbags are available");
-    }
-
-}
--- src/org/tns/cap/c2tc/jrdeveloper/SpeedLimit.java
+++
@@ -1,7 +0,0 @@
-package org.tns.cap.c2tc.jrdeveloper;
-
-public class SpeedLimit {
-	public void showSpeedLimit() {
-        System.out.println("Speed limit is 120 km/h");
-    }
-}
